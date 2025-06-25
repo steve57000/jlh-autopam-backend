c5d10f374e0cf30b90cf0d4b857b14e424b52339
steve <steve57b@hotmail.fr>
2025-06-25 14:25:16 +0200

    Initial commit: structure Spring Boot + configs Docker

---
+++ src/main/java/com/jlh/jlhautopambackend/JlhApplication.java
@@ -0,0 +1,13 @@
+package com.jlh.jlhautopambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JlhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jlh/jlhautopambackend/JlhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlh.jlhautopambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
